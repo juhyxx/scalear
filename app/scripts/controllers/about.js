e64e98bd64662184613ad3ec483962e231028873
juhyxx <juhy@rezatyrakety.cz>
2014-10-30 23:22:22 +0100

    Init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scalearApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the scalearApp
+ */
+angular.module('scalearApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
