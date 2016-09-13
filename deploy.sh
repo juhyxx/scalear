b7ba745bf5d0d6a2bba631dee303c6547191cdce
Miroslav Juhy Juhos <juhy@rezatyrakety.cz>
2016-09-13 20:16:30 +0200

    Travis

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ git config user.name "travis"
 git config user.email "travis"
 git add .
 git commit -m "Travis to ${TRAVIS_BUILD_NUMBER}"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master > /dev/null 2>&1
+git push --force --quiet "https://${GITHUB_API_KEY}@${REPO}" master > /dev/null 2>&1
